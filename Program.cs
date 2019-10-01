e6abfd6d15fa3f9559f37467e16af289889c5a43
Jacob Davis <jacob.marquez.davis@hotmail.com>
2019-10-01 11:39:25 -0500

    First commit!

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace HelloRevature
+{
+    public struct TestStruct
+    {
+        int x;
+
+        public void init(int y = 0)
+        {
+            x = y;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestStruct f = new TestStruct();
+            f.init();
+            string b = "1";
+            int a = int.Parse(b);
+            Console.WriteLine(b);
+        }
+    }
+}
