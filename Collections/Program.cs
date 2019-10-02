20a23a2cbb5e20f03b498c48725e10eb3c572880
Jacob Davis <jacob.marquez.davis@hotmail.com>
2019-10-02 09:18:24 -0500

    Update

---
+++ Collections/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Lists();
+        }
+        static void Lists()
+        {
+            ArrayList al = new ArrayList();
+            al.Add(1);
+            al.Add(11.1);
+
+            List<int> ls = new List<int>();
+        }
+    }
+}
